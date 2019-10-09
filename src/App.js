7960c26c8fd290de872eda8de68f0186e8a67c59
Sridatta <sridatta.pasumarthy@oracle.com>
2019-10-09 20:48:12 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useTransition, animated } from "react-spring";
 
-function App() {
+import { BATTING_AVERAGES } from "./data";
+import { gradients } from "./utils/constants";
+
+import "./styles/styles.css";
+
+function App({ finalCount, initialCount, time }) {
+  const [matchCount, setCount] = useState(initialCount);
+  useEffect(
+    () =>
+      void setInterval(() => {
+        const increment = a => (a <= finalCount - 1 ? a + 1 : a);
+        setCount(increment);
+      }, time),
+    [finalCount, time]
+  );
+  const rows = BATTING_AVERAGES[matchCount].map(a => ({
+    ...a,
+    image:
+      a.country.split("/").length > 1
+        ? gradients[a.country.split("/")[1]]
+        : gradients[a.country]
+  }));
+
+  let height = 0;
+  const transitions = useTransition(
+    rows
+      .slice(0, 8)
+      .map(data => ({ ...data, y: (height += data.height) - data.height })),
+    d => d.playerId,
+    {
+      from: { height: 0, opacity: 0 },
+      leave: { height: 0, opacity: 0 },
+      enter: ({ y, height }) => ({ y, height, opacity: 1 }),
+      update: ({ y, height }) => ({ y, height })
+    }
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "75%" }}>
+      <h2>{`Batting Averages After ${matchCount} Matches`}</h2>
+      <div className="list" style={{ height }}>
+        {transitions.map(({ item, props: { y, ...rest }, key }, index) => {
+          return (
+            <animated.div
+              key={key}
+              className="card"
+              style={{
+                zIndex: rows.length - index,
+                transform: y.interpolate(y => `translate3d(0,${y}px,0)`),
+                ...rest
+              }}
+            >
+              <div
+                className="cell"
+                style={{ width: `${Number(item.average) * 10}px` }}
+              >
+                <div
+                  className="details"
+                  style={{ backgroundImage: item.image }}
+                >
+                  <span>{`${item.playerName} (${item.average})`}</span>
+                </div>
+              </div>
+            </animated.div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const gradients = {
+  AUS: "linear-gradient(120deg, #f6d365 0%, #fda085 100%)",
+  PAK: "linear-gradient(to top, #96fbc4 0%, #f9f586 100%)",
+  INDIA: "linear-gradient(135deg, #c3cfe2 0%, #c3cfe2 100%)",
+  WI: "linear-gradient(135deg, #f093fb 0%, #f5576c 100%)",
+  ENG: "linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)",
+  SA: "linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)",
+  NZ: "linear-gradient(135deg, #d299c2 0%, #fef9d7 100%)",
+  SL: "linear-gradient(135deg, #ebc0fd 0%, #d9ded8 100%)"
+};
