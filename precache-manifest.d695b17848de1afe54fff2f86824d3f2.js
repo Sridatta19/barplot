1f1c8a9a2fee41368caf4da3558b35cb4a283538
Sridatta <sridatta.pasumarthy@oracle.com>
2019-12-15 11:17:37 +0530

    Updates

---
+++ precache-manifest.d695b17848de1afe54fff2f86824d3f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4385532da4d8b69cd616a328c5e2d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f3e1037400763272de0",
+    "url": "/static/css/main.45a885d0.chunk.css"
+  },
+  {
+    "revision": "18bf19b72a5bacba73a2",
+    "url": "/static/js/2.6a46590c.chunk.js"
+  },
+  {
+    "revision": "4f3e1037400763272de0",
+    "url": "/static/js/main.41db0dbe.chunk.js"
+  },
+  {
+    "revision": "e99d4cf21835b8b54574",
+    "url": "/static/js/runtime-main.c0306b74.js"
+  }
+]);
